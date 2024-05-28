37ad2cc75dd393cfb3db5ca5de370da9762e1956
foxik-nd <froxik77430@outlook.fr>
2024-05-28 15:44:13 +0200

    Home page ok

--- WEBAPP/paris_jo_2024/src/components/views/Home.jsx
+++ WEBAPP/paris_jo_2024/src/components/views/Home.jsx
@@ -4,9 +4,17 @@ import '../styles/home.css'
 function Home() {
   return (
     <div>
-      <h1>Page d'accueil</h1>
+      <h1>Accueil</h1>
+      <h2>Les Jeux Olympique</h2>
+      <h3>Histoire</h3>
+      <p>Les Jeux Olympiques ont une histoire riche qui remonte à l'Antiquité. Les premiers Jeux Olympiques ont eu lieu en 776 av. J.-C. en Grèce et étaient dédiés à Zeus.<br/>Ils ont été abandonnés au IVe siècle après J.-C., mais ont été ravivés à la fin du XIXe siècle par Pierre de Coubertin. Les premiers Jeux Olympiques modernes ont eu lieu en 1896 à Athènes et depuis lors, ils ont eu lieu tous les quatre ans, sauf pendant les guerres mondiales.<br/>Ils ont évolué pour inclure de nouveaux sports et catégories, comme les Jeux Paralympiques. Les Jeux Olympiques ont été le théâtre de moments historiques et de performances exceptionnelles et continuent d'être un symbole de l'excellence sportive et de l'unité mondiale.</p>
+      <h3>Paris 2024</h3>
+      <p>Les Jeux Olympiques d'été de 2024 auront lieu à Paris, en France, du 26 juillet au 11 août 2024. Cela marquera la troisième fois que Paris accueillera les Jeux Olympiques.<br/>Le thème des Jeux est "L'amour des Jeux" et comprendra 329 épreuves dans 33 sports différents, y compris de nouveaux sports comme le surf, l'escalade sportive et le skateboard. Les épreuves se dérouleront dans des sites emblématiques de Paris et en dehors de la ville. Les Jeux Olympiques de Paris 2024 seront également un événement durable et responsable, avec pour objectif de réduire l'empreinte carbone des Jeux de 50% par rapport aux Jeux précédents. Les Jeux Olympiques de Paris 2024 promettent d'être un événement sportif exceptionnel dans une ville magnifique et emblématique.</p>
+      <h2>Prédire les résultats</h2>
+      <a href='/prediction-gagnant' class="myButton">Prediction</a>
     </div>
   );
 }
 
 export default Home;
+  
